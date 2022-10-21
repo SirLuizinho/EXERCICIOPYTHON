44134c151c9b8fe4b80cf626012841b52133948c
Luiz Felipe <luizfsp33@gmail.com>
2022-10-21 19:56:28 -0300

    Alteração de exercícios

---
+++ 2MensagemBoasVindas.py
@@ -0,0 +1,2 @@
+nome = str(input("\033[32;1mOlá! Qual o seu nome? "))
+print(f"Olá \033[31;1m{nome}!\033[32;1m É um grande prazer te conhecer!")
---
+++ 3SomandoDoisNumeros.py
@@ -0,0 +1,6 @@
+print('\033[31;1mSomando dois números')
+n1 = int(input("\033[30;1mDigite um número: "))
+n2 = int(input("Digite outro número: "))
+n3 = n1 + n2
+
+print(f"A soma entre \033[31;1m{n1} \033[30;1me \033[31;1m{n2} \033[30;1mé \033[31;1m{n3}")
